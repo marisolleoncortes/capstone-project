e4a29eaab36289a8054fba9bf15a4684521911cb
Marisol Cortes <marisolleoncortes@gmail.com>
2021-04-10 15:26:00 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "Stay Fresh",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "12XKJTcGwRLqZtL4J-lrOiz0qVBcl6VEyXTO0Hm_eXQ",
+        spaceId: "4yl7w504wmhg",
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "12345",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
